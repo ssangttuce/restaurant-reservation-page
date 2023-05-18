67a9a88d58331f455a65f71141d6c3d88b8a42d6
ssangttuce <sanghoo1023@gmail.com>
2023-05-18 10:33:10 +0900

    feat: HTML, JS code modification

--- JS/genTime.js
+++ JS/genTime.js
@@ -1,22 +1,22 @@
 function genHour() {
   let list = document.getElementsByClassName('hour');
-  for (let i = 0; i < list.length; i++) {
-    for (let j = 0; j < 24; j++) {
-      let h = document.createElement('option');
-      h.value = j;
-      h.innerText = j;
-      list[i].appendChild(h);
-    }
+  list[0].appendChild(document.createElement('option'));
+  for (let j = 0; j < 24; j++) {
+    let h = document.createElement('option');
+    h.value = j;
+    h.innerText = j;
+    list[0].appendChild(h);
   }
 }
 
 function genMinute(min) {
   let list = document.getElementsByClassName('minute');
+  list[0].appendChild(document.createElement('option'));
   for (let i = 0; i < list.length; i++) {
     let m = document.createElement('option');
     m.value = min;
     m.innerText = min;
-    list[i].appendChild(m);
+    list[0].appendChild(m);
   }
 }
 genHour();
--- JS/getReservation.js
+++ JS/getReservation.js
@@ -3,16 +3,29 @@ function getTable() {
   table.innerText = '1';
 }
 
+function genTable() {
+  let table = document.getElementById('table-num');
+  table.appendChild(document.createElement('option'));
+  for (let i = 1; i < 17; i++) {
+    let numOption = document.createElement('option');
+    numOption.value = i;
+    numOption.innerText = i;
+    table.appendChild(numOption);
+  }
+}
+
 function ccButton() {
   const cc = document.createElement('button');
   console.log(typeof cc);
 }
 
-function getReservation(userName) {
-  let row = "<tr><td></td><td></td><td></td><td></td><td class='ccButton'>  </td></tr>";
+function getReservation() {
+  let row = '<tr><td></td><td></td><td></td><td></td><td></td></tr>';
   let searchTable = document.getElementsByTagName('tbody');
   for (let i = 0; i < 20; i++) {
     searchTable[0].innerHTML += row;
     //마지막 td에 cc추가하기
   }
 }
+
+// genTable();
