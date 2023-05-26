a05e58a5e98a7d0b5dee657d0875c6f937a2bbec
ssangttuce <sanghoo1023@gmail.com>
2023-05-26 18:03:05 +0900

    add: reservation cancel page add as modal

--- JS/getReservation.js
+++ JS/getReservation.js
@@ -18,23 +18,33 @@ function genTableNum() {
   }
 }
 
-function ccButton() {
+function ccButton(cancelNum) {
   // cc: cancel and change
   let c1 = document.createElement('button');
-  let c2 = document.createElement('button');
+  let c2 = document.createElement('input');
   c1.innerText = '변경';
-  c2.innerText = '취소';
   c1.className = 'change-button';
-  c2.className = 'cancel-button';
-  // c1.onclick = "location.href = '../html/shiftTable.html'";
+  c2.setAttribute('type', 'checkbox');
+  c2.id = `cancel-${cancelNum}`;
+  let c2Label = document.createElement('label');
+  c2Label.setAttribute(`for`, `cancel-${cancelNum}`);
+  c2Label.innerText = '취소';
+
   c1.onclick = () => {
     location.href = '../html/changeReservation.html';
   };
   c2.onclick = () => {
-    location.href = '../html/cancelReservation.html';
+    let cancelModal = document.getElementById('cancel-check-modal');
+    cancelModal.style.visibility = 'visible';
+
+    for (let i = 0; ; i++) {
+      let cancelBtn = document.getElementById(`cancel-${i}`);
+      if (cancelBtn == null) break;
+      cancelBtn.disabled = true;
+    }
   };
-  // c2: 취소 확인 modal 띄운후 server로 취소 요청
-  return [c1, c2];
+
+  return [c1, c2, c2Label];
 }
 
 function searchReservationByName() {
@@ -43,29 +53,37 @@ function searchReservationByName() {
   if (searchResult === null) {
     return;
   }
+  let i = 0;
   for (let list of getDummyReservation(20)) {
     let res = document.createElement('tr');
-    res.innerHTML += `<td>` + `${list.a}` + `</td>`;
-    res.innerHTML += `<td>` + `${list.b}` + `</td>`;
-    res.innerHTML += `<td>` + `${list.c}` + `</td>`;
-    res.innerHTML += `<td>` + `${list.d}` + `</td>`;
-    res.innerHTML += `<td></td>`;
-
-    for (let cc of ccButton()) {
+    let resHtml = `<td>` + `${list.a}` + `</td>`;
+    resHtml += `<td>` + `${list.b}` + `</td>`;
+    resHtml += `<td>` + `${list.c}` + `</td>`;
+    resHtml += `<td>` + `${list.d}` + `</td>`;
+    res.innerHTML = resHtml + `<td></td>`;
+    for (let cc of ccButton(i)) {
       res.lastElementChild.appendChild(cc);
     }
+    i++;
     searchResult.appendChild(res);
+
+    res.lastElementChild.lastElementChild.onclick = () => {
+      let cancelReservation = document.createElement('tr');
+      cancelReservation.innerHTML = resHtml;
+      document.getElementById('cancel-reservation').appendChild(cancelReservation);
+    };
+  }
+
+  function getDummyReservation(cnt) {
+    let dummy = [];
+
+    for (let i = 0; i < cnt; i++) {
+      let reservation = { a: `a+${i}`, b: `b+${i}`, c: `c+${i}`, d: `d+${i}` };
+      dummy.push(reservation);
+    }
+    return dummy;
   }
 }
 
-function getDummyReservation(cnt) {
-  let dummy = [];
-
-  for (let i = 0; i < cnt; i++) {
-    let reservation = { a: `a+${i}`, b: `b+${i}`, c: `c+${i}`, d: `d+${i}` };
-    dummy.push(reservation);
-  }
-  return dummy;
-}
 searchReservationByName();
 genTableNum();
