cfb6c42fded0fa3f90b5c154a993eea218c55b4f
ssangttuce <sanghoo1023@gmail.com>
2023-07-31 14:31:18 +0900

    link reservation storage and main page, it resopnses to date

--- JS/main-page.js
+++ JS/main-page.js
@@ -1,40 +1,67 @@
+import { searchByDate, openningHours, tableCount } from './reservationStorage.js';
+
 const todayDate = new Date().toJSON().slice(0, 10).replace(/-/g, '-');
 
+let searchDate = document.querySelector('input[type = "date"]');
+searchDate.setAttribute('value', todayDate);
+searchDate.addEventListener('change', showMainTable);
+
+initialSetting();
+showMainTable();
+
 function initialSetting() {
-  function genTimeline(openTime) {
+  function genTimeline() {
     const timeline = document.querySelector('.reservation-table-head tr');
-    console.log(timeline);
-    for (let i = 0; i < openTime; i++) {
-      let th = document.createElement('th');
-      th.className = 'reservation-table-header';
-      th.innerText = `${parseInt(i / 2)}:${i % 2 === 0 ? '00' : '30'}`;
-      timeline.appendChild(th);
+    for (let i of openningHours()) {
+      let th00 = document.createElement('th');
+      th00.setAttribute('class', 'reservation-table-header');
+      th00.innerText = `${i}:00`;
+      timeline.appendChild(th00);
+
+      let th30 = document.createElement('th');
+      th30.setAttribute('class', 'reservation-table-header');
+      th30.innerText = `${i}:30`;
+      timeline.appendChild(th30);
     }
   }
 
-  function addTable(tableCount) {
-    //서버에서 받아오기
+  genTimeline();
+}
+
+function showMainTable() {
+  function addTable() {
+    searchByDate();
     const res = document.querySelector('.reservation-table-instance');
+    res.innerHTML = '';
+
     for (let i = 1; i <= tableCount; i++) {
       let table = document.createElement('tr');
-      table.className = 'table-row';
+      table.setAttribute('class', 'table-row');
       table.innerHTML = `<td id="table-${i}">` + `T${i}` + '</td>';
       res.appendChild(table);
-      getRBlocks(table);
+      getRBlocks(i, table);
+    }
+
+    function getRBlocks(tableNum, table) {
+      for (let i of openningHours()) {
+        let td00 = document.createElement('td');
+        let td30 = document.createElement('td');
+        td00.setAttribute('class', 'available');
+        td30.setAttribute('class', 'available');
+
+        for (let reservation of searchByDate()) {
+          if (tableNum === reservation.tablenumber) {
+            if (`${reservation.hour}` == `${i}`) {
+              reservation.minute === '00'
+                ? td00.setAttribute('class', 'unavailable')
+                : td30.setAttribute('class', 'unavailable');
+            }
+          }
+        }
+        table.appendChild(td00);
+        table.appendChild(td30);
+      }
     }
   }
-
-  function getRBlocks(table) {
-    for (let i = 0; i < 48; i++) {
-      let td = document.createElement('td');
-      td.className = parseInt(Math.random() * 2 + 1) === 1 ? 'available' : 'unavailable';
-      // server에서 조회한 값으로 대체할 예정
-      table.appendChild(td);
-    }
-  }
-
-  let searchDate = document.querySelector('input[type = "date"]');
-  searchDate.setAttribute('value', todayDate);
-  genTimeline(48);
-  addTable(20);
+  addTable();
 }
--- JS/reservationStorage.js
+++ JS/reservationStorage.js
@@ -2,7 +2,79 @@ let reservationList = [
   {
     name: '',
     phone: '010-1234-5678',
-    date: '2023-07-22',
+    date: '2023-07-30',
+    hour: '10',
+    minute: '30',
+    personnel: 4,
+    tablenumber: 6,
+  },
+  {
+    name: '',
+    phone: '010-1234-5678',
+    date: '2023-07-31',
+    hour: '11',
+    minute: '30',
+    personnel: 4,
+    tablenumber: 10,
+  },
+  {
+    name: '',
+    phone: '010-1234-5678',
+    date: '2023-07-31',
+    hour: '13',
+    minute: '30',
+    personnel: 4,
+    tablenumber: 6,
+  },
+  {
+    name: '',
+    phone: '010-1234-5678',
+    date: '2023-07-30',
+    hour: '10',
+    minute: '30',
+    personnel: 4,
+    tablenumber: 6,
+  },
+  {
+    name: '',
+    phone: '010-1234-5678',
+    date: '2023-07-31',
+    hour: '10',
+    minute: '30',
+    personnel: 4,
+    tablenumber: 6,
+  },
+  {
+    name: '',
+    phone: '010-1234-5678',
+    date: '2023-07-30',
+    hour: '10',
+    minute: '30',
+    personnel: 4,
+    tablenumber: 6,
+  },
+  {
+    name: '',
+    phone: '010-1234-5678',
+    date: '2023-07-30',
+    hour: '10',
+    minute: '30',
+    personnel: 4,
+    tablenumber: 6,
+  },
+  {
+    name: '',
+    phone: '010-1234-5678',
+    date: '2023-07-30',
+    hour: '10',
+    minute: '30',
+    personnel: 4,
+    tablenumber: 6,
+  },
+  {
+    name: '',
+    phone: '010-1234-5678',
+    date: '2023-07-30',
     hour: '10',
     minute: '30',
     personnel: 4,
@@ -11,6 +83,18 @@ let reservationList = [
 ];
 
 const formNameList = ['name', 'phone', 'date', 'hour', 'minute', 'personnel', 'tablenumber'];
+
+const tableCount = 16;
+const open = 10;
+const close = 20;
+
+const openningHours = () => {
+  let array = [];
+  for (let time = open; time < close; time++) {
+    array.push(time);
+  }
+  return array;
+};
 
 function setReservation(event) {
   event.preventDefault();
@@ -27,7 +111,17 @@ function setReservation(event) {
   for (let formName of formNameList) {
     newReservation[`${formName}`] = document.querySelector(`[name = "${formName}"]`).value;
   }
-  reservationList.push(newReservation);
 }
 
-export { reservationList, setReservation };
+function searchByDate() {
+  const date = document.querySelector('[name = "search-date"]').value;
+  const reserved = [];
+  for (let reservation of reservationList) {
+    if (reservation.date == date) {
+      reserved.push(reservation);
+    }
+  }
+  return reserved;
+}
+
+export { reservationList, setReservation, searchByDate, openningHours, tableCount };
